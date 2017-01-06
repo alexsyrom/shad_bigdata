b3798aaeae5b2b75ff3e1f8d8cbf88e2f531f2cb
alexsyrom <alexsyrom@yandex.ru>
2017-01-07 00:06:57 +0300

    sync

--- hw1/metrics/sync.sh
+++ hw1/metrics/sync.sh
@@ -2,3 +2,4 @@
 rsync -avzh ~/hdfs/hw1/metrics/users_by_country/results ~/src/shad_bigdata/hw1/metrics/users_by_country/ 
 rsync -avzh ~/hdfs/hw1/metrics/total_users/results ~/src/shad_bigdata/hw1/metrics/total_users/ 
 rsync -avzh ~/hdfs/hw1/metrics/average_session_time/results ~/src/shad_bigdata/hw1/metrics/average_session_time/ 
+rsync -avzh ~/hdfs/hw1/metrics/profile_liked_three_days/results ~/src/shad_bigdata/hw1/metrics/profile_liked_three_days/ 
