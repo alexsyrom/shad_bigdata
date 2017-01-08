37913e1d0ff7db5f5a4dfeeb2ed111d517beefc2
alexsyrom <alexsyrom@yandex.ru>
2017-01-08 16:15:40 +0300

    work version

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-spark-submit 
+spark-submit \
       --master yarn-client \
       --num-executors 2 \
       --executor-cores 1 \
       --executor-memory 2048m \
-      stream_metric.py \
+      stream_metrics.py \
       --topic bigdatashad-2016 \
       --zookeeper hadoop2-10:2181
--- hw3/stream_metrics.py
+++ hw3/stream_metrics.py
@@ -14,23 +14,25 @@ from builtins import (
 
 from pyspark import SparkContext
 from pyspark import SparkConf
+from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
 import re
 import argparse
 import os
 import sys
+import collections
 
 
-def update_func(new_values, state):
+def update_function(new_values, state):
     if state is None:
         state = 0
     return sum(new_values, state)
 
 
 def print_counts(rdd):
-    counts = rdd.collectAsMap()
-    print("15_second_count={15}; 60_second_count={60}; total_count={total};".format(**counts))
+    counts = collections.Counter(rdd.collectAsMap())
+    print("15_second_count={n15}; 60_second_count={n60}; total_count={total};".format(**counts))
 
 
 if __name__ == '__main__':
@@ -47,16 +49,16 @@ if __name__ == '__main__':
     logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
     logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
 
-    kvs = KafkaUtils.createStream(ssc, agrs.zookeeper, "consumer-HW3StreamMetricsComputer", {args.topic: 4})
+    kvs = KafkaUtils.createStream(ssc, args.zookeeper, "consumer-HW3StreamMetricsComputer", {args.topic: 4})
     lines = kvs.map(lambda x: x[1])
     parsed_lines = lines.map(lambda line: list(map(''.join, re.findall(r'\"(.*?)\"|\[(.*?)\]|(\S+)', line))))
     unsuccessful_queries = parsed_lines.filter(lambda words: words[5] != '200')
-    15_second_count = unsuccessful_queries.count()
-    60_second_count = unsuccessful_queries.countByWindow(60, 15)
-    total_count = 15_second_count.map(lambda x: ('total', x)).updateStateByKey(update_function)
-    counts = ssc.union([15_second_count.map(lambda x: ('15', x)),
-                        60_second_count.map(lambda x: ('60', x)),
-                        total_count])
+    n15_second_count = unsuccessful_queries.count()
+    n60_second_count = unsuccessful_queries.countByWindow(60, 15)
+    total_count = n15_second_count.map(lambda x: ('total', x)).updateStateByKey(update_function)
+    counts = ssc.union(n15_second_count.map(lambda x: ('n15', x)),
+                       n60_second_count.map(lambda x: ('n60', x)),
+                       total_count)
     counts.foreachRDD(print_counts)
 
     ssc.start()
